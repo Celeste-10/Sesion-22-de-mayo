7590c9d98841369b978d7f63c88eb944f19eb0ce
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2026-05-24 11:12:58 -0600

    Se añadio el proyecto

---
+++ src/main/java/ni/edu/uam/EjemploOX/modelo/Estudiante.java
@@ -0,0 +1,26 @@
+package ni.edu.uam.EjemploOX.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombres;
+    private String apellidos;
+    private LocalDate fechaNacimiento;
+
+}
